b62cffd247018ab7dcadb53f5bfc545ac122ca7f
Morgan Jones <me@sweetuk.net>
2023-03-07 13:53:18 +0000

    Initial Commit

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-// Get active navbar page
-let path = window.location.pathname.split("/").filter((x) => x)[0];
-
-let active = (path === undefined ? "home" : path) + "-nav";
-
-const navElement = document.getElementById(active);
-navElement.classList.add("active");
