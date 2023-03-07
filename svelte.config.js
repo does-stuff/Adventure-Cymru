138d6eeb27bf5e1d777ee3009bf9c8a37cf63f68
Morgan Jones <me@sweetuk.net>
2023-03-07 15:05:58 +0000

    Config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-auto";
+import { dirname } from "path";
+import preprocess from "svelte-preprocess";
+import { fileURLToPath } from "url";
+
+const filePath = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: preprocess({
+    scss: {
+      prependData: `@use "${filePath}/src/styles/vars" as *;`,
+    },
+  }),
 
-	kit: {
-		adapter: adapter(),
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
