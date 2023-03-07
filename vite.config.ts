96265cc47bd6aa85951e983ac6ce81d87ec2e005
Morgan Jones <me@sweetuk.net>
2023-03-07 14:02:46 +0000

    Basics

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			"$lib": resolve("./src/lib"),
+			"$componentd": resolve("./src/lib/components"),
+		}
+	}
 });
