1b6af34a6a9cbf01ee65bf40984a799bb40785a4
Morgan Jones <me@sweetuk.net>
2023-03-07 15:06:11 +0000

    Finished Navbar

---
+++ src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const active = "/" + ($page.url.pathname.split("/")[0] || "");
+
+  interface Page {
+    name: string;
+    path:
+      | `/${string}`
+      | `http${"" | "s"}://${string}.${string}${"" | `/${string}`}`;
+  }
+
+  export let pages: Page[] = [
+    { name: "Home", path: "/" },
+    { name: "Activities", path: "/activities" },
+    { name: "Development", path: "/development" },
+    {
+      name: "Source Code",
+      path: "https://github.com/morganjones-dev/Adventure-Cymru/",
+    },
+  ];
+</script>
+
+<nav class="nav">
+  <ul class="list">
+    {#each pages as page}
+      <li class="linkWrapper">
+        <a class="link" class:active={active === page.path} href={page.path}>{page.name}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
+<style lang="scss">
+  .nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: red;
+    width: 100%;
+    height: 64px;
+  }
+
+  .list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    gap: 20px;
+  }
+
+  .link {
+    color: white;
+    text-decoration: none;
+    font-family: "Poppins", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    transition: color 0.25s ease-in-out;
+
+    &.active,
+    &:hover {
+      color: $accent;
+    }
+  }
+</style>
